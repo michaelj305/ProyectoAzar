12311fc3346956901f4002b5ec4e15712709cfb6
JuanMa0818 <juanm.mejiap1@uqvirtual.edu.co>
2026-04-24 17:15:20 -0500

    Hola lesbicas

---
+++ lib/sorteo.ex
@@ -0,0 +1,18 @@
+defmodule AzarSorteo do
+  @moduledoc """
+  Define la estructura de datos para un sorteo de Azar S.A.
+  """
+
+ # @derive {Jason.Encoder, only: [:id, :nombre, :fecha, :precio_billete, :fracciones_totales, :cantidad_billetes, :premios, :billetes, :estado]}
+  defstruct [
+    id: nil,                  # Un identificador único (ej: "SOR-101")
+    nombre: "",               # Nombre del sorteo
+    fecha: nil,               # Fecha programada [cite: 28]
+    precio_billete: 0,        # Valor del billete completo
+    fracciones_totales: 0,    # Cantidad de fracciones por billete [cite: 30]
+    cantidad_billetes: 0,     # Cuántos billetes existen (con número único) [cite: 31]
+    premios: [],              # Lista de premios asociados [cite: 34, 60]
+    billetes: %{},            # Mapa para rastrear quién compró qué número
+    estado: :pendiente        # :pendiente, :realizado o :cancelado [cite: 35]
+  ]
+end
---
+++ mix.exs
@@ -0,0 +1,26 @@
+defmodule ProyectoAzar.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :proyecto_azar,
+      version: "0.1.0",
+      elixir: "~> 1.19",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+  [
+    {:jason, "~> 1.4"}
+  ]
+end
+end
